599c4d6908c13fb92b1540bf528d1b3038396b4f
zlatanglor <oshovskiy.d@gmail.com>
2022-02-06 20:00:59 +0200

    add animation, hover

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ document.querySelector('header form').addEventListener('change',(event) =>{
         document.documentElement.classList.remove('dark', 'light', 'backendless');
         document.documentElement.classList.add(event.target.value);
     }
+
+});
+var select = document.querySelector('header form select');
+select.addEventListener('change', function() {
+    document.documentElement.classList.remove('dark', 'light', 'backendless');
+    document.documentElement.classList.add(event.target.value);
+});
+var select1 = document.querySelector('header form button');
+select1.addEventListener('click', function() {
+    document.documentElement.classList.remove('dark', 'light', 'backendless');
+    document.documentElement.classList.add(event.target.value);
+    console.log(target.value);
 });
