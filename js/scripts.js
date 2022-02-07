0849f2fac5941029df2be9de7ef6e3d3584ad4dd
zlatanglor <oshovskiy.d@gmail.com>
2022-02-07 12:40:55 +0200

    add select theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
-document.querySelector('header form').addEventListener('change',(event) =>{
+document.querySelector('header form.desktop').addEventListener('change',(event) =>{
     if (event.target.nodeName === 'INPUT'){
         document.documentElement.classList.remove('dark', 'light', 'backendless');
         document.documentElement.classList.add(event.target.value);
     }
 
 });
-var select = document.querySelector('header form select');
-select.addEventListener('change', function() {
+var Mobile = document.querySelector('header form.mobile select');
+Mobile.addEventListener('change', function() {
     document.documentElement.classList.remove('dark', 'light', 'backendless');
     document.documentElement.classList.add(event.target.value);
 });
-var select1 = document.querySelector('header form button');
-select1.addEventListener('click', function() {
+var Tablet = document.querySelector('header form.tablet ');
+Tablet.addEventListener('click', function() {
     document.documentElement.classList.remove('dark', 'light', 'backendless');
     document.documentElement.classList.add(event.target.value);
-    console.log(target.value);
 });
+
+
